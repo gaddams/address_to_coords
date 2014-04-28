d150a3c743ebe7df53ab9a85e5a48ade533dd6a1
gaddams <sgaddam2015@kellogg.northwestern.edu>
2014-04-27 20:16:52 -0500

    updates to api part 1

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -12,5 +12,15 @@ class AddressesController < ApplicationController
     # parsed_data = ?
     # @latitude = ?
     # @longitude = ?
+
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    results = parsed_data["results"]
+    first = results[0]
+    geometry = first["geometry"]
+    location = geometry["location"]
+    @latitude = location["lat"]
+    @longitude = location["lng"]
   end
 end
